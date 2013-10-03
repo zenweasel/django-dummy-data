07b3758d95dfae3ea87995e5ae68d1ad820efb36
Brent Hoover <brent@thebuddhalodge.com>
2013-10-03 15:50:34 -0700

    Return correct status codes

--- gisproject/destination/views.py
+++ gisproject/destination/views.py
@@ -3,13 +3,13 @@ from django.template.response import TemplateResponse
 
 
 def direct_to_template(request):
-    return TemplateResponse(request, 'home.html', status=404)
+    return TemplateResponse(request, 'home.html', status=200)
 
 
 def login(request):
     post_vars = request.POST
     if post_vars.get('username') != 'parkme' or post_vars.get('password') != 'parkme':
-        return TemplateResponse(request, 'fail.html', status=401)
+        return TemplateResponse(request, 'fail.html', status=403)
     else:
-        return TemplateResponse(request, 'success.html', status=401)
+        return TemplateResponse(request, 'success.html', status=200)
 
